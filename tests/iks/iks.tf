6d2df2ce8621b638aaf4b3111bf880443986054f
Vikrant Balyan (vvb) <vvb@cisco.com>
2021-08-28 00:37:08 +0530

    add tf that is run as a part of tests

---
+++ tests/iks/iks.tf
@@ -0,0 +1,50 @@
+resource "intersight_kubernetes_cluster_profile" "kcp1" {
+  name = "dummy_kcp_tf"
+  organization {
+    object_type = "organization.Organization"
+    moid = var.organization
+  }
+}
+
+data "intersight_kubernetes_cluster_profile" "kcp1" {
+  depends_on = [
+    intersight_kubernetes_cluster_profile.kcp1
+  ]
+  name = "dummy_kcp_tf"
+}
+
+resource "intersight_kubernetes_addon_policy" "kap1" {
+  name = "dummy-kap1"
+  organization {
+    moid = var.organization
+    object_type = "organization.Organization"
+  }
+}
+
+resource "intersight_kubernetes_addon_policy" "kap2" {
+  name = "dummy-kap2"
+  organization {
+    moid = var.organization
+    object_type = "organization.Organization"
+  }
+}
+
+resource "intersight_kubernetes_cluster_addon_profile" "iks-terra-addons" {
+  name = "iks-terra-addons"
+  addons {
+    addon_policy {
+      moid = intersight_kubernetes_addon_policy.kap1.moid
+      object_type = "cmrf.Cmrf"
+    }
+  }
+  addons {
+    addon_policy {
+      moid = intersight_kubernetes_addon_policy.kap2.moid
+      object_type = "cmrf.Cmrf"
+    }
+  }
+  organization {
+    object_type = "organization.Organization"
+    moid = var.organization
+  }
+}
