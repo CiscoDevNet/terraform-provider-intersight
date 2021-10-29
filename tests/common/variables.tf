8353b5393d03cff86be959130de9abd00c987f56
Vikrant Balyan <vvb@users.noreply.github.com>
2021-10-29 22:55:17 +0530

    define required vars (#141)

--- tests/common/variables.tf
+++ tests/common/variables.tf
@@ -12,3 +12,13 @@ variable "endpoint" {
   type = string
   description = "Endpoint URL"
 }
+
+variable "privatekey" {
+  type = string
+  description = "Private key base64 encoded value"
+}
+
+variable "pem_certificate" {
+  type = string
+  description = "Pem certificate base64 encoded value"
+}
