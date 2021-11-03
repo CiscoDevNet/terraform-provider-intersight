d0b18c2233192ef474785c4484222032d1bc4f18
Meghana R <Meghana.R@Maplelabs.com>
2021-11-03 23:49:21 +0530

    Testcase to test float conversin

---
+++ tests/common/capability_equipment_physical_def.tf
@@ -0,0 +1,11 @@
+resource "intersight_capability_equipment_physical_def" "capability_equipment_physical_def1" {
+  name          = "capability_equipment_physical_def1"
+  pid           = "UCS-FI-6454"
+  sku           = "CON-NCF4P-FI6454CH"
+  depth         = 57.1
+  height        = 4.4
+  max_power     = 650
+  nominal_power = 260
+  weight        = 10.10
+  width         = 43.9
+}
