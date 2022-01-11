3ca99ca804167860ccda48b0b5af8cb46e83bb33
Meghana R <Meghana.R@Maplelabs.com>
2022-01-12 00:29:37 +0530

    Updated config to create server profile template before referencing

--- tests/only_post/main.tf
+++ tests/only_post/main.tf
@@ -18,6 +18,70 @@ data "intersight_organization_organization" "default" {
 }
 
 
+#Boot precision policy for creating server profile template
+resource "intersight_boot_precision_policy" "boot_precision1" {
+  name                     = "boot_precision2"
+  description              = "test policy"
+  configured_boot_mode     = "Legacy"
+  enforce_uefi_secure_boot = false
+  organization {
+    object_type = "organization.Organization"
+    moid        = intersight_organization_organization.default.moid
+  }
+  boot_devices {
+    enabled     = true
+    name        = "scu-device-hdd"
+    object_type = "boot.LocalDisk"
+    additional_properties = jsonencode({
+      Slot = "MRAID"
+      Bootloader = {
+        Description = ""
+        Name        = ""
+        ObjectType  = "boot.Bootloader"
+        Path        = ""
+      }
+    })
+  }
+  boot_devices {
+    enabled     = true
+    name        = "NIIODCIMCDVD"
+    object_type = "boot.VirtualMedia"
+    additional_properties = jsonencode({
+      Subtype = "cimc-mapped-dvd"
+    })
+  }
+  boot_devices {
+    enabled     = true
+    name        = "hdd"
+    object_type = "boot.LocalDisk"
+    additional_properties = jsonencode({
+      Slot = "MRAID"
+      Bootloader = {
+        Description = ""
+        Name        = ""
+        ObjectType  = "boot.Bootloader"
+        Path        = ""
+      }
+    })
+  }
+}
+
+
+resource "intersight_server_profile_template" "template1" {
+  name            = "server_profile_template1"
+  description     = "demo server profile template"
+  target_platform = "FIAttached"
+   organization {
+     object_type = "organization.Organization"
+     moid        = intersight_organization_organization.default.moid
+   }
+   policy_bucket {
+     moid        = intersight_boot_precision_policy.boot_precision1.moid
+     object_type = "boot.PrecisionPolicy"
+   }
+}
+
+
 #resource "intersight_hcl_compatibility_status" "comptibility_status" {
 #  request_type = "CheckCompatibility"
 #}
@@ -30,7 +94,7 @@ resource "intersight_bulk_mo_cloner" "clone_server1"{
        sources {
                class_id = "server.ProfileTemplate"
                object_type = "server.ProfileTemplate"
-               moid = "61dd4d1477696e2d315f374f"
+               moid = intersight_server_profile_template.template1.moid
        }
        targets {
                class_id = "server.Profile"
