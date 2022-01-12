87a78a46d90a8f3553128ce96dda3ead2f80fc45
Meghana R <Meghana.R@Maplelabs.com>
2022-01-12 15:26:24 +0530

    Removing kubernetes cluster from testing due to backend issues

--- tests/common/kubernetes_cluster.tf
+++
@@ -1,9 +0,0 @@
-
-resource "intersight_kubernetes_cluster" "kubernetes_cluster1" {
-  connection_status = "Connected"
-  name              = "kubernetes_cluster1"
-  organization {
-    object_type = "organization.Organization"
-    moid        = data.intersight_organization_organization.default.results.0.moid
-  }
-}
