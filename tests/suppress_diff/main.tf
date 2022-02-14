9fa3de0d68df76ea4b03b6ab6896fd8c15670935
Meghana R <Meghana.R@Maplelabs.com>
2022-02-14 13:39:25 +0530

    Adding vnic_adapter to test roce_settings. Also removed replication from hyperflex_cluster_profile to test ignore behavior

--- tests/suppress_diff/main.tf
+++ tests/suppress_diff/main.tf
@@ -25,7 +25,6 @@ resource "intersight_hyperflex_cluster_profile" "hyperflex_cluster_profile1" {
   mgmt_ip_address    = "10.225.68.237"
   mac_address_prefix = "00:25:B5:D5"
   mgmt_platform      = "EDGE"
-  replication        = 3
   description        = "This is hyperflex cluster profile"
   tags {
     key   = "test"
@@ -53,5 +52,55 @@ resource "intersight_networkconfig_policy" "network_config1" {
   organization {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.default.results.0.moid
+  }
+}
+
+resource "intersight_vnic_eth_adapter_policy" "vnic_eth_adapter_policy_1" {
+  name                    = "v_eth_adapter1"
+  rss_settings            = true
+  uplink_failback_timeout = 5
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.default.results.0.moid
+  }
+  vxlan_settings {
+    enabled = false
+  }
+
+  nvgre_settings {
+    enabled = true
+  }
+
+  arfs_settings {
+    enabled = true
+  }
+
+  roce_settings {
+	enabled = false
+  }
+
+  interrupt_settings {
+    coalescing_time = 125
+    coalescing_type = "MIN"
+    nr_count        = 4
+    mode            = "MSI"
+  }
+  completion_queue_settings {
+    nr_count  = 4
+    ring_size = 1
+  }
+  rx_queue_settings {
+    nr_count  = 4
+    ring_size = 512
+  }
+  tx_queue_settings {
+    nr_count  = 4
+    ring_size = 512
+  }
+  tcp_offload_settings {
+    large_receive = true
+    large_send    = true
+    rx_checksum   = true
+    tx_checksum   = true
   }
 }
