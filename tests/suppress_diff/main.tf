a57c14f268bf72ff4cc605a19b6f2bd6856a468b
Meghana R <Meghana.R@Maplelabs.com>
2022-02-15 09:56:32 +0530

    Testing passphrase

--- tests/suppress_diff/main.tf
+++ tests/suppress_diff/main.tf
@@ -102,3 +102,21 @@ resource "intersight_vnic_eth_adapter_policy" "vnic_eth_adapter_policy_1" {
     tx_checksum   = true
   }
 }
+
+resource "intersight_kubernetes_virtual_machine_infra_config_policy" "vm_infra_config_policy" {
+  name        = "TFC-Test-1"
+  description = "VM Infrastructure Config Policy used for Kubernetes deployment to vSphere"
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.default.results[0].moid
+  }
+  vm_config {
+    object_type = "kubernetes.EsxiVirtualMachineInfraConfig"
+	interfaces = ["8", "4"]
+    additional_properties = jsonencode({
+      Cluster    =  "abc"
+      Datastore  = "datastore"
+      Passphrase =  "password123"
+    })
+  }
+}
