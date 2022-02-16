3e8c0342f693e256205d59a8737a49e39117931a
Meghana R <Meghana.R@Maplelabs.com>
2022-02-16 11:59:47 +0530

    Testing Inband Ip pool

--- tests/suppress_diff/main.tf
+++ tests/suppress_diff/main.tf
@@ -119,3 +119,21 @@ resource "intersight_kubernetes_virtual_machine_infra_config_policy" "vm_infra_c
     })
   }
 }
+
+
+resource "intersight_access_policy" "access1" {
+  name        = "access1"
+  description = "demo imc access policy"
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.default.results.0.moid
+  }
+  configuration_type{
+	configure_inband = false
+	configure_out_of_band = true
+  }
+  out_of_band_ip_pool {
+	object_type = "ippool.Pool"
+	moid = "61e90cf96962752d31a40826"
+  }
+}
