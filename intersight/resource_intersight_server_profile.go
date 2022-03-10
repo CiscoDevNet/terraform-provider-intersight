e7910ed5bd012eeb1ed6627c03ade1e08351a17f
Meghana R <Meghana.R@Maplelabs.com>
2022-03-10 13:53:05 +0530

    Bug fix

--- intersight/resource_intersight_server_profile.go
+++ intersight/resource_intersight_server_profile.go
@@ -1835,7 +1835,7 @@ func resourceServerProfileCreate(c context.Context, d *schema.ResourceData, meta
 			}
 			return diag.Errorf("error occurred while updating ServerProfile: %s", responseErr.Error())
 		}
-		log.Printf("Moid: %s", resultMo.GetMoid())
+		log.Printf("Moid: %s", result.GetMoid())
 	}
 	log.Printf("Moid: %s", resultMo.GetMoid())
 	d.SetId(resultMo.GetMoid())
