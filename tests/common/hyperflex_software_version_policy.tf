c038c71c01a63bc9ec2cf5babee8950eb3910532
Meghana R <Meghana.R@Maplelabs.com>
2022-04-22 12:33:27 +0530

    Reading version from datasource

--- tests/common/hyperflex_software_version_policy.tf
+++ tests/common/hyperflex_software_version_policy.tf
@@ -1,6 +1,9 @@
+data "intersight_hyperflex_hxdp_version" "hxdp_version"{	
+}
+
 
 resource "intersight_hyperflex_software_version_policy" "hyperflex_software_version_policy1" {
-  hxdp_version = "5.0(1b)"
+  hxdp_version = data.intersight_hyperflex_hxdp_version.hxdp_version.results[length(data.intersight_hyperflex_hxdp_version.hxdp_version.results)-1].nr_version
   organization {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.default.results.0.moid
