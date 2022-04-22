08af885a3b1d062c15b116890f0d68038d36e099
Meghana R <Meghana.R@Maplelabs.com>
2022-04-22 10:32:01 +0530

    Updated unsupported hxdp version

--- tests/common/hyperflex_software_version_policy.tf
+++ tests/common/hyperflex_software_version_policy.tf
@@ -1,6 +1,6 @@
 
 resource "intersight_hyperflex_software_version_policy" "hyperflex_software_version_policy1" {
-  hxdp_version = "4.5(2a)"
+  hxdp_version = "5.0(1b)"
   organization {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.default.results.0.moid
