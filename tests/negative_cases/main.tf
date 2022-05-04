2a222ec3785f3b98dcae5cb9f837abb6746a11ac
Meghana R <Meghana.R@Maplelabs.com>
2022-05-04 16:08:58 +0530

    Adding negative testcases

---
+++ tests/negative_cases/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    intersight = {
+      source = "CiscoDevNet/intersight"
+      version = "1.0.27"
+    }
+  }
+}
+
+provider "intersight" {
+  apikey = var.api_key
+  secretkey = var.secretkey
+  endpoint = var.endpoint
+}
+
+data "intersight_organization_organization" "default" {
+  name  = "default"
+}
