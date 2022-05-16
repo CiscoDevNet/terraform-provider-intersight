2e58a7c9b8cd3817050a68bd7e9a1a208f774733
Meghana R <53509179+meghana-rajashekar@users.noreply.github.com>
2022-05-16 11:08:10 +0530

    Removing data-source (#206)
    Removing datasource whose match no more exists in the account.

--- tests/data_sources/license_reservation_ops.tf
+++ tests/data_sources/license_reservation_ops.tf
@@ -6,25 +6,6 @@ output "auth_code_installed_false" {
   value = data.intersight_license_license_reservation_op.auth_code_installed_false.results.0
 }
 
-
-data "intersight_license_license_reservation_op" "generate_request_code_false" {
-  generate_request_code = false
-}
-
-output "generate_request_code_false" {
-  value = data.intersight_license_license_reservation_op.generate_request_code_false.results.0
-}
-
-
-data "intersight_license_license_reservation_op" "generate_return_code_false" {
-  generate_return_code = false
-}
-
-output "generate_return_code_false" {
-  value = data.intersight_license_license_reservation_op.generate_return_code_false.results.0
-}
-
-
 data "intersight_license_license_reservation_op" "license_reservation_op" {
 }
 
