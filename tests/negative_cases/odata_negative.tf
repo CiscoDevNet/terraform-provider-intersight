86dd21b8bb0d76569ffc2da8ecc7f4ad51597a0a
Meghana R <Meghana.R@Maplelabs.com>
2022-08-18 11:18:13 +0530

    Changing block type for testing

--- tests/negative_cases/odata_negative.tf
+++ tests/negative_cases/odata_negative.tf
@@ -1,51 +1,51 @@
 data "intersight_aaa_audit_record" "odata_top_and_skip_negative_values"{
-	odata {
+	odata = {
 	 skip = -90
 	 top = -20
 	}
 }
 
 data "intersight_search_search_item" "odata_skip_negative_value"{
-	odata {
+	odata = {
 	 skip = -200
 	}
 }
 
 data "intersight_compute_rack_unit" "odata_skip_negative_top_positive_value"{
-	odata {
+	odata = {
 	 skip = -200
 	 top = 50
 	}
 }
 
 data "intersight_smtp_policy" "odata_skip_negative_top_positive_zero_result_mo"{
-	odata {
+	odata = {
 	 skip = -600
 	 top = 10
 	}
 }
 
 data "intersight_access_policy" "odata_skip_negative_top_positive_zero_result_mo"{
-	odata {
+	odata = {
 	 skip = -950
 	}
 }
 
 data "intersight_smtp_policy" "odata_top_skip_zero_results"{
-	odata {
+	odata = {
 	 top = 10
 	 skip = 23
 	}
 }
 
 data "intersight_hyperflex_software_distribution_version" "odata_at1"{
-	odata {
+	odata = {
 	at = "Version eq '1.3(1b)'"      
 	}
 }
 
 data "intersight_hyperflex_hxdp_version" "odata_at2"{
-	odata {
+	odata = {
 	at = "Version eq '4.0(2e)'"     
 	}
 }
