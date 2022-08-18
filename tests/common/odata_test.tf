a558eefced6c492c483ef9f2f4f65ca73103e29c
Meghana R <Meghana.R@Maplelabs.com>
2022-08-18 12:49:56 +0530

    Change to block

--- tests/common/odata_test.tf
+++ tests/common/odata_test.tf
@@ -1,188 +1,188 @@
 data "intersight_search_search_item" "odata_top_skip_value1"{
-	odata = {
+	odata {
 	 top = 143
 	 skip = 200
 	}
 }
 
 data "intersight_aaa_audit_record" "odata_top_skip_small_value1"{
-	odata = {
+	odata {
 	 top = 80
 	 skip = 40
 	}
 }
 
 data "intersight_workflow_task_info" "odata_top_skip_small_value2"{
-	odata = {
+	odata {
 	 top = 30
 	 skip = 60
 	}
 }
 
 data "intersight_search_search_item" "odata_top_and_skip_small_value3"{
-	odata = {
+	odata {
 	 top = 20
 	 skip = 10
 	}
 }
 
 data "intersight_workflow_task_info" "odata_top_skip_value2"{
-	odata = {
+	odata {
 	 top = 230
 	 skip = 150
 	}
 }
 
 data "intersight_aaa_audit_record" "odata_top_and_skip_value3"{
-	odata = {
+	odata {
 	 top = 70
 	 skip = 65
 	}
 }
 
 data "intersight_workflow_task_info" "odata_top_and_skip_value4"{
-	odata = {
+	odata {
 	 top = 174
 	 skip = 133
 	}
 }
 
 data "intersight_search_search_item" "odata_top_and_skip_value5"{
-	odata = {
+	odata {
 	 top = 440
 	 skip = 250
 	}
 }
 
 data "intersight_aaa_audit_record" "odata_top_and_skip_value6"{
-	odata = {
+	odata {
 	 top = 748
 	 skip = 660
 	}
 }
 
 data "intersight_aaa_audit_record" "odata_top_and_skip_large_values1"{
-	odata = {
+	odata {
 	 top = 1000
 	 skip = 500
 	}
 }
 
 data "intersight_aaa_audit_record" "odata_top_value1"{
-	odata = {
+	odata {
 	 top = 12
 	}
 }
 
 data "intersight_appliance_app_status" "odata_skip_value1"{
-	odata = {
+	odata {
 	 skip = 60
 	}
 }
 
 data "intersight_appliance_app_status" "odata_top_value2"{
-	odata = {
+	odata {
 	 top = 80
 	}
 }
 
 data "intersight_search_search_item" "odata_top_value3"{
-	odata = {
+	odata {
 	 top = 890
 	}
 }
 
 data "intersight_workflow_task_metadata" "odata_skip_large_value1"{
-	odata = {
+	odata {
 	 skip = 200
 	}
 }
 
 data "intersight_workflow_task_definition" "odata_skip_large_value2"{
-	odata = {
+	odata {
 		skip = 1200
 	}
 }
 
 data "intersight_appliance_app_status" "odata_top_zero_value"{
-	odata = {
+	odata {
 	 top = 0
 	}
 }
 
 data "intersight_appliance_app_status" "odata_skip_zero_value"{
-	odata = {
+	odata {
 	 skip = 0
 	}
 }
 
 data "intersight_workflow_workflow_info" "odata_top_negative_value"{
-	odata = {
+	odata {
 	 top = -20
 	}
 }
 
 data "intersight_aaa_audit_record" "odata_top_and_skip_value8"{
-	odata = {
+	odata {
 	 skip = 500
 	 top = 700
 	}
 }
 
 data "intersight_aaa_audit_record" "odata_top_and_skip_value9"{
-	odata = {
+	odata {
 	 top = 1300
 	 skip = 500
 	}
 }
 
 data "intersight_workflow_task_definition" "odata_count_true_value"{
-	odata = {
+	odata {
 	 nr_count = true
 	}
 }
 
 data "intersight_workflow_task_metadata" "odata_count_false_value"{
-	odata = {
+	odata {
 	 nr_count = false
 	}
 }
 
 data "intersight_workflow_task_metadata" "odata_count_filter"{
-	odata = {
+	odata {
 	 nr_count = true
 	 filter = "Label eq 'Restart IMC'"
 	}
 }
 
 data "intersight_iam_role" "odata_count_orderby"{
-	odata = {
+	odata {
 	 nr_count = false
 	 orderby = "CreateTime"
 	}
 }
 
 data "intersight_hyperflex_software_distribution_component" "odata_orderby_descending"{
-	odata = {
+	odata {
 	 orderby = "CreateTime desc"
 	}
 }
 
 data "intersight_adapter_ext_eth_interface" "odata_orderby_ascending_select"{
-	odata = {
+	odata {
 	 orderby = "CreateTime"
 	 nr_select = "CreateTime,MacAddress"
 	}
 }
 
 data "intersight_adapter_ext_eth_interface" "odata_orderby_ascending"{
-	odata = {
+	odata {
 	 orderby = "Moid"
 	}
 }
 
 data "intersight_appliance_app_status" "odata_orderby_top_skip"{
-	odata = {
+	odata {
 	 orderby = "ModTime desc"
 	 top = 10
 	 skip = 50
@@ -190,19 +190,19 @@ data "intersight_appliance_app_status" "odata_orderby_top_skip"{
 }
 
 data "intersight_compute_rack_unit" "odata_filter_equal"{
-	odata = {
+	odata {
 	 filter = "Name eq 'C125-WZP22360YWC'"
 	}
 }
 
 data "intersight_adapter_host_eth_interface" "odata_filter_not_equal"{
-	odata = {
+	odata {
 	 filter = "Moid ne '6167ee1576752d32341e2df3'"
 	}
 }
 
 data "intersight_adapter_host_eth_interface" "odata_filter_orderby_select"{
-	odata = {
+	odata {
 	filter = "Moid ne '6167ee1576752d32341e2df3'"
 	orderby = "Name"
 	nr_select = "Name,ModTime"
@@ -210,38 +210,38 @@ data "intersight_adapter_host_eth_interface" "odata_filter_orderby_select"{
 }
 
 data "intersight_compute_rack_unit" "odata_filter_greater_than"{
-	odata = {
+	odata {
 	filter = "AvailableMemory gt 98304"
 	}
 }
 
 data "intersight_compute_rack_unit" "odata_filter_greater_than_or_equal"{
-	odata = {
+	odata {
 	filter = "AvailableMemory ge 98304"
 	}
 }
 
 data "intersight_compute_rack_unit" "odata_filter_lesser_than"{
-	odata = {
+	odata {
 	filter = "AvailableMemory lt 98304"
 	}
 }
 
 data "intersight_search_search_item" "odata_filter_less_than_or_equal"{
-	odata = {
+	odata {
 	filter = "MemorySize le 50"
 	}
 }
 
 data "intersight_compute_rack_unit" "odata_filter_and_additional_property"{
-	odata = {
+	odata {
 		filter = "AvailableMemory gt 65000"
 	}
 	additional_properties = jsonencode({ "Model" = "UCSC-C240-M5L"})
 }
 
 data "intersight_compute_rack_unit" "odata_filter_not_and_top_and_select"{
-	odata = {
+	odata {
 		nr_select = "Model,Serial" 
 		top=10 
 		filter = "not(Model eq 'UCSC-C125' or Model eq 'HX220C-M5S')"
@@ -249,44 +249,44 @@ data "intersight_compute_rack_unit" "odata_filter_not_and_top_and_select"{
 }
 
 data "intersight_compute_rack_unit" "odata_filter_in"{
-	odata = {
+	odata {
 		filter = "Model in ('UCSC-C125', 'UCSC-C240-M5L')"
 	}
 }
 
 data "intersight_compute_rack_unit" "odata_filter_contains"{
-	odata = {
+	odata {
 		filter = "contains(Model,'C240')"
 	}
 }
 
 data "intersight_server_profile" "odata_filter_startswith"{
-	odata = {
+	odata {
 		filter = "startswith(Name,'cloned')"
 	}
 }
 
 data "intersight_server_profile" "odata_filter_endswith"{
-	odata = {
+	odata {
 		filter = "endswith(Name,'test')"
 	}
 }
 
 data "intersight_appliance_app_status" "odata_top_filter_empty_array"{
-	odata = {
+	odata {
 		filter = "not(Tags/any())"
 		top = 5
 	}
 }
 
 data "intersight_workflow_workflow_info" "odata_top_filter_not_empty_array"{
-	odata = {
+	odata {
 		filter = "Message/any()"
 	}
 }
 
 data "intersight_compute_rack_unit" "odata_top_skip_select"{
-	odata = {
+	odata {
 		top = 20 
 		skip = 1 
 		nr_select = "Model,Serial"
@@ -294,14 +294,14 @@ data "intersight_compute_rack_unit" "odata_top_skip_select"{
 }
 
 data "intersight_adapter_host_fc_interface" "odata_top_inlinecount_allpages"{
-	odata = {
+	odata {
 		top = 2 
 		inlinecount = "allpages"
 	}
 }
 
 data "intersight_adapter_host_fc_interface" "odata_top_inlinecount_none"{
-	odata = {
+	odata {
 		top = 3
 		inlinecount = "none"
 	}
