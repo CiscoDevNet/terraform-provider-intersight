9121e50bfaf8756847dd9b9bc882019044117264
Meghana R <Meghana.R@Maplelabs.com>
2022-08-19 14:32:27 +0530

    Removing zero results testcases as they might not always be true

--- tests/negative_odata/odata_negatives.tf
+++ tests/negative_odata/odata_negatives.tf
@@ -18,36 +18,3 @@ data "intersight_compute_rack_unit" "odata_skip_negative_top_positive_value"{
 	}
 }
 
-data "intersight_smtp_policy" "odata_skip_negative_top_positive_zero_result_mo"{
-	odata {
-	 skip = -600
-	 top = 10
-	}
-}
-
-data "intersight_access_policy" "odata_skip_negative_top_positive_zero_result_mo"{
-	odata {
-	 skip = -950
-	}
-}
-
-data "intersight_smtp_policy" "odata_top_skip_zero_results"{
-	odata {
-	 top = 10
-	 skip = 23
-	}
-}
-
-data "intersight_hyperflex_software_distribution_version" "odata_at1"{
-	odata {
-	at = "Version eq '1.3(1b)'"      
-	}
-}
-
-data "intersight_hyperflex_hxdp_version" "odata_at2"{
-	odata {
-	at = "Version eq '4.0(2e)'"     
-	}
-}
-
-
