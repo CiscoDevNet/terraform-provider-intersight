bf8f37f6b44f90a1273168a8665546763f054b14
MEGHANA R <meghanar1998@gmail.com>
2022-09-05 10:08:45 +0530

    Updating the property as per openapi spec file changes

--- tests/negative_cases/tests.tf
+++ tests/negative_cases/tests.tf
@@ -6,7 +6,7 @@ resource "intersight_sol_policy" "enum_int" {
 
 # Enum property for string
 resource "intersight_workflow_workflow_info" "enum_string" {
-    workflow_meta_type = "systemdefined"
+    wait_reason = "ratelimit"
 }
 
 # Maximum integer
