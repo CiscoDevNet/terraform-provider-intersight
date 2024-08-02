0e456bee519dafb79b7b6893d0f75ac87e992361
Vikrant Balyan (vvb) <vvb@cisco.com>
2024-08-02 18:29:39 +0530

    adds required property

--- tests/common/kubernetes_cluster_addon_profile.tf
+++ tests/common/kubernetes_cluster_addon_profile.tf
@@ -4,11 +4,13 @@ resource "intersight_kubernetes_cluster_addon_profile" "iks1" {
 
   addons {
     addon_policy {
+      object_type = "kubernetes.AddonPolicy"
       moid = intersight_kubernetes_addon_policy.kubernetes_addon_policy1.moid
     }
   }
   addons {
     addon_policy {
+      object_type = "kubernetes.AddonPolicy"
       moid = intersight_kubernetes_addon_policy.kubernetes_addon_policy1.moid
     }
   }
