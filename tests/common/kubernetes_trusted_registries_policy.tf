3a7c26816edd116cdc77b89e6259335266cdb738
JINDAT BARADIA GIRISH -X (jingiris - XORIANT CORPORATION at Cisco) <jingiris@cisco.com>
2024-08-13 22:44:59 +0530

    Updating Tests

--- tests/common/kubernetes_trusted_registries_policy.tf
+++ tests/common/kubernetes_trusted_registries_policy.tf
@@ -2,4 +2,8 @@
 resource "intersight_kubernetes_trusted_registries_policy" "kubernetes_trusted_registries_policy1" {
   description = "kubernetes trusted registries policy"
   name        = "kubernetes_trusted_registries_policy1"
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.default.results.0.moid
+  }
 }
--- tests/common/macpool_pool.tf
+++ tests/common/macpool_pool.tf
@@ -6,4 +6,8 @@ resource "intersight_macpool_pool" "macpool_pool1" {
     from        = "00:25:B5:9d:68:16"
     to          = "00:25:B5:9d:68:40"
   }
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.default.results.0.moid
+  }
 }
