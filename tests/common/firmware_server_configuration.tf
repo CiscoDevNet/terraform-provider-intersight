c44453fe703590aa3a1b4c276e17ae991a9adddf
the-akhil-nair <sam1232007@gmail.com>
2024-09-13 17:38:52 +0530

    Added the resource for firmware server configuration

---
+++ tests/common/firmware_server_configuration.tf
@@ -0,0 +1,36 @@
+resource "intersight_firmware_server_configuration_utility_distributable" "scu1" {
+  name = "SCU-6.0.4c nfs"
+  nr_source {
+    object_type = "softwarerepository.CifsServer"
+    additional_properties = jsonencode({
+      FileLocation = "10.10.10.20/iso.iso"
+      RemoteIp     = "10.10.10.1"
+      RemoteShare  = "/path/to/remote/share"
+      RemoteFile   = "/path/to/remote/file"
+      Username     = "user"
+      Password     = "ChangeMe"
+      MountOption  = "sec=ntlm"
+    })
+  }
+  vendor     = "Cisco"
+  nr_version = "6.0.(4c)"
+  supported_models = [
+    "C-series"
+  ]
+  description = "Cisco SCU-6.0(4c)"
+  catalog {
+    moid        = data.intersight_softwarerepository_catalog.catalog.results[0].moid
+    object_type = "softwarerepository.Catalog"
+  }
+}
+
+data "intersight_organization_organization" "organization" {
+  name = "v3keytest"
+}
+
+data "intersight_softwarerepository_catalog" "catalog"{
+  organization {
+    class_id = "organization.Organization"
+    moid = data.intersight_organization_organization.organization.id
+  }
+} 
