58712a391a2e1e84da2e804e0887ff093eb5dc74
the-akhil-nair <sam1232007@gmail.com>
2024-09-13 15:07:27 +0530

    Test cases related to complex filters

---
+++ tests/data_sources/complex_filters.tf
@@ -0,0 +1,18 @@
+data "intersight_organization_organization" "organization" {
+  name = "default"
+}
+
+output "default_organization" {
+  value = data.intersight_organization_organization.organization.results.0
+}
+
+data "intersight_softwarerepository_catalog" "sw_catalog" {
+  organization {
+    moid = data.intersight_organization_organization.organization.id
+  }
+  account_moid = data.intersight_organization_organization.organization.results.0.account_moid
+}
+
+output "catalog_in_default_organization" {
+  value = data.intersight_softwarerepository_catalog.sw_catalog.results.0
+}
