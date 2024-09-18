eb1e544916341d770fa48d1f432dcaa101c8f0b3
JINDAT BARADIA GIRISH -X (jingiris - XORIANT CORPORATION at Cisco) <jingiris@cisco.com>
2024-09-18 17:08:45 +0530

    Testcase covering Tags

--- tests/common/server_profile.tf
+++ tests/common/server_profile.tf
@@ -6,8 +6,20 @@ resource "intersight_server_profile" "tf_server_common" {
     key   = "server"
     value = "demo"
   }
+  tags {
+    key   = "project"
+    value = "cloud_migration"
+  }
+  tags {
+    key   = "Environment"
+    value = "production"
+  }
+   tags {
+    key   = "Application"
+    value = "WebService"
+  }
   organization {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.default.results.0.moid
-  }
+  } 
 }
